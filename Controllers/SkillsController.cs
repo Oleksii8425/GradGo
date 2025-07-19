a649841197df9a997121442ba96b5248685af00d
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-07-19 14:00:33 +0100

    Add Delete endpoint to SkillsController

--- Controllers/SkillsController.cs
+++ Controllers/SkillsController.cs
@@ -32,7 +32,7 @@ public class SkillsController : ControllerBase
     }
 
     [HttpPost]
-    public async Task<IActionResult> Create(Skill skill)
+    public async Task<IActionResult> CreateSkill(Skill skill)
     {
         _context.Skills.Add(skill);
         await _context.SaveChangesAsync();
@@ -41,7 +41,7 @@ public class SkillsController : ControllerBase
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> Update(int id, Skill skill)
+    public async Task<IActionResult> UpdateSkill(int id, Skill skill)
     {
         if (id != skill.Id)
             return BadRequest();
@@ -60,6 +60,19 @@ public class SkillsController : ControllerBase
             else
                 throw;
         }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteSkill(int id)
+    {
+        var skill = await _context.Skills.FindAsync(id);
+        if (skill == null)
+            return NotFound();
+
+        _context.Skills.Remove(skill);
+        await _context.SaveChangesAsync();
 
         return NoContent();
     }
--- obj/Debug/net8.0/GradGo.AssemblyInfo.cs
+++ obj/Debug/net8.0/GradGo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradGo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0225b11599750e37cba5e63b7f891282418977ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc258021b192b4ddad626b0e921dd4e8380ec5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradGo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradGo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
