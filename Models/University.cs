c9a6480fd7e0314524f47b0accc934bbb7beec8f
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-07-21 16:32:43 +0100

    Make Country navigation property nullable in University

--- Models/University.cs
+++ Models/University.cs
@@ -3,8 +3,9 @@ namespace GradGo.Models
     public class University
     {
         public int Id { get; set; }
+        public int CountryId { get; set; }
         public required string Name { get; set; }
         public required string City { get; set; }
-        public required string Country { get; set; }
+        public Country? Country { get; set; }
     }
 }
