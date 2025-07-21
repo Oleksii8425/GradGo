ac13ba724567caec3f2722fe2d118ca07d1b346b
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-07-21 13:20:37 +0100

    Add SkillMapper

---
+++ Mappers/SkillMapper.cs
@@ -0,0 +1,26 @@
+using GradGo.DTOs;
+using GradGo.Models;
+
+namespace GradGo.Mappers
+{
+    public static class SkillMapper
+    {
+        public static SkillDto ToSkillDto(this Skill skill)
+        {
+            return new SkillDto(
+                skill.Id,
+                skill.Title
+            );
+        }
+
+        public static Skill ToSkill(this SkillCreateDto dto)
+        {
+            return new Skill { Title = dto.Title };
+        }
+
+        public static void UpdateSkillFromDto(this Skill skill, SkillUpdateDto dto)
+        {
+            skill.Title = dto.Title;
+        }
+    }
+}
