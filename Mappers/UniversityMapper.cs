b5c160cd909872cceae9a206ac795e4d07782f3c
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-07-21 17:17:17 +0100

    Add UniversityMapper

---
+++ Mappers/UniversityMapper.cs
@@ -0,0 +1,35 @@
+using GradGo.DTOs;
+using GradGo.Models;
+
+namespace GradGo.Mappers
+{
+    public static class UniversityMapper
+    {
+        public static UniversityDto ToUniversityDto(this University university)
+        {
+            return new UniversityDto(
+                university.Id,
+                university.Name,
+                university.City,
+                university.Country.Name
+            );
+        }
+
+        public static University ToUniversity(this UniversityCreateDto dto)
+        {
+            return new University
+            {
+                Name = dto.Name,
+                City = dto.City,
+                CountryId = dto.CountryId
+            };
+        }
+
+        public static void UpdateUniversityFromDto(this University university, UniversityUpdateDto dto)
+        {
+            if (dto.Name != null) university.Name = dto.Name;
+            if (dto.City != null) university.City = dto.City;
+            if (dto.CountryId.HasValue) university.CountryId = dto.CountryId.Value;
+        }
+    }
+}
