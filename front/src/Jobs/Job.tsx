039c8bcaefbf8c38e6c4c8f513d06a22fca7eaee
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-08-07 14:19:54 +0100

    Add Job component

---
+++ front/src/Jobs/Job.tsx
@@ -0,0 +1,36 @@
+interface JobProps {
+  title: string;
+  employer: string;
+  type: string;
+  city: string;
+  country: Country;
+  onClick?: () => void;
+}
+
+interface Country {
+  id: number,
+  countryCode: string,
+  name: string,
+  phoneCode: string,
+  currencyCode: string,
+  currencySymbol: string
+}
+
+function Job({
+  title,
+  employer,
+  type,
+  city,
+  country,
+  onClick
+}: JobProps) {
+  return (
+    <div className="card" onClick={onClick}>
+      <h1 className="card__title">{title}</h1>
+      <p>{employer}</p>
+      <p>{type} - {city}, {country.name}</p>
+    </div>
+  );
+}
+
+export default Job;
