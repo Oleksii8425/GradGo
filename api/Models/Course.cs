2b1361b7361678af6cb21ba3ed90ede2afac2af4
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-08-13 20:20:27 +0100

    Refactor Course model to use nullable properties for Name and Degree for improved flexibility; add a University navigation property

--- api/Models/Course.cs
+++ api/Models/Course.cs
@@ -4,8 +4,9 @@ namespace GradGo.Models
     {
         public int Id { get; set; }
         public Guid UniveristyId { get; set; }
-        public required string Name { get; set; }
-        public required Degree Degree { get; set; }
+        public string Name { get; set; } = null!;
+        public Degree Degree { get; set; }
+        public University University { get; set; } = null!;
         public DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }
         public ICollection<Jobseeker> Jobseekers { get; set; } = new List<Jobseeker>();
