5f7cc399561f69da26b0f79e1fec8f1b92afadd6
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-09-02 20:43:20 +0100

    Add CountrySelector component for job creation form

---
+++ front/app/components/JobCreate/CountrySelector.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import type { Country } from "~/types";
+
+interface CountrySelectorProps {
+  onChange: (selected: Country | null) => void;
+}
+
+function CountrySelector({ onChange }: CountrySelectorProps) {
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [selectedCountry, setSelectedCountry] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const res = await fetch("http://localhost:5272/countries");
+
+        if (!res.ok) throw new Error("Failed to fetch countries");
+
+        const data = await res.json();
+        setCountries(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
+  useEffect(() => {
+    const country = selectedCountry
+      ? countries.find(c => c.id === selectedCountry) || null
+      : null;
+
+    onChange(country);
+  }, [selectedCountry, countries, onChange]);
+
+  return (
+    <select
+      name="countries"
+      id="countries"
+      className="border rounded px-2 py-2 bg-slate-900 text-gray-300"
+      value={selectedCountry || ""}
+      required
+      onChange={(e) => {
+        const value = Number(e.target.value);
+
+        if (!value) return;
+
+        if (selectedCountry != value) {
+          setSelectedCountry(value);
+        }
+      }}
+    >
+      <option value="" disabled>
+        -- Select a country --
+      </option>
+      {countries.map((country: Country) => (
+        <option key={country.id} value={country.id}>
+          {country.name}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export default CountrySelector;
