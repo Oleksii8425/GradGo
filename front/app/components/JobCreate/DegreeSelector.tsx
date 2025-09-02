8c9da229484c66d2a42f6b5903c541729a114607
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-09-02 20:43:32 +0100

    Add DegreeSelector component for job creation form

---
+++ front/app/components/JobCreate/DegreeSelector.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { Degree } from "~/types";
+
+interface DegreeSelectorProps {
+  onChange: (degree: Degree | null) => void;
+};
+
+function DegreeSelector({ onChange }: DegreeSelectorProps) {
+  const [degree, setDegree] = useState<Degree | null>(null);
+
+  useEffect(() => {
+    onChange(degree);
+  }, [degree, onChange]);
+
+  return (
+    <select
+      name="degree"
+      id="degree"
+      className="border rounded px-2 py-2 bg-slate-900 text-gray-300"
+      value={degree ?? ""}
+      required
+      onChange={(e) => {
+        const value = Number(e.target.value);
+        setDegree(isNaN(value) ? null : (value as Degree));
+      }}
+    >
+      <option value="" disabled>
+        -- Select a required degree --
+      </option>
+      <option value={Number(Degree.Bachelors)}>Bachelors</option>
+      <option value={Number(Degree.Masters)}>Masters</option>
+      <option value={Number(Degree.Doctors)}>Doctors</option>
+    </select>
+  );
+}
+
+export default DegreeSelector;
