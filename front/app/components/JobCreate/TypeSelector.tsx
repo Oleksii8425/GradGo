071b426dd82910b44b023029438fd4c24654e567
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-09-02 20:43:41 +0100

    Add DegreeSelector component for job creation form

---
+++ front/app/components/JobCreate/TypeSelector.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { JobType } from "~/types";
+
+interface TypeSelectorProps {
+  onChange: (type: JobType | null) => void;
+};
+
+function TypeSelector({ onChange }: TypeSelectorProps) {
+  const [type, setType] = useState<JobType | null>(null);
+
+  useEffect(() => {
+    onChange(type);
+  }, [type, onChange]);
+
+  return (
+    <select
+      name="jobTypes"
+      id="jobTypes"
+      className="border rounded px-2 py-2 bg-slate-900 text-gray-300"
+      value={type ?? ""}
+      required
+      onChange={(e) => {
+        const value = Number(e.target.value);
+        setType(isNaN(value) ? null : (value as JobType));
+      }}
+    >
+      <option value="" disabled>
+        -- Select a job type --
+      </option>
+      <option value={Number(JobType.OnSite)}>On Site</option>
+      <option value={Number(JobType.Remote)}>Remote</option>
+      <option value={Number(JobType.Hybrid)}>Hybrid</option>
+    </select>
+  );
+}
+
+export default TypeSelector;
