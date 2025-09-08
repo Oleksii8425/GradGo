3cca3f62d13563b8381e09b4469ccd8effd81e4a
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-09-08 16:49:37 +0100

    Add more job details tp JobDetails component and arrange them nicely

--- front/app/components/JobDetails.tsx
+++ front/app/components/JobDetails.tsx
@@ -6,26 +6,37 @@ interface JobDetailsProps {
 
 function JobDetails({ job }: JobDetailsProps) {
   return (
-    <div className="flex-3 max-h-full flex flex-col overflow-scroll rounded-lg p-4 border border-slate-500">
-      <h1 className="text-2xl font-bold">{job.title}
-        <a href="">
-          - {job.emplyerName}
-        </a>
-      </h1>
-      <p>{job.city}, {job.countryName}</p>
-      <p>{job.salary}{job.currencySymbol}</p>
-      <hr className="mt-2 mb-4" />
-      <div className="flex gap-4 mb-4">
+    <div className="flex-2 max-h-full flex flex-col items-start overflow-scroll rounded-lg p-4 border border-slate-500">
+      <p className="text-xs">{job.emplyerName}</p>
+      <h1 className="text-2xl font-bold">{job.title}</h1>
+      <p className="text-xs mb-2">
+        {job.city}, {job.countryName}
+        {" · "}
+        {job.applicantsNo} applicants
+      </p>
+      <div className="flex gap-2 mt-1 *:cursor-default">
+        <p className="p-2 rounded-full border">
+          {job.salary}{job.currencySymbol}
+        </p>
+        <p className="p-2 rounded-full border">
+          {job.type}
+        </p>
+      </div>
+      <button className="rounded-full p-2 mt-2 bg-slate-700 hover:bg-slate-800">
+        Apply
+      </button>
+      <h1 className="mt-4 mb-2 text-2xl font-bold">About the job</h1>
+      {/*<div className="flex gap-2 mb-4">
         {
           job.skills.map((skill) =>
-            <p className="p-3 rounded-full bg-slate-700 hover:bg-slate-800">
+            <p className="p-2 rounded-full bg-slate-700">
               {skill.title}
             </p>
           )
         }
-      </div>
+      </div>*/}
       <div className="flex flex-col overflow-scroll">
-        <p className="text-justify">{job.description}</p>
+        <p className="text-justify pr-2">{job.description}</p>
       </div>
     </div>
   );
