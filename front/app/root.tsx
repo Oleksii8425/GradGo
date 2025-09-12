074f60d720a1842973d8f01148f48d73ac19612d
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-09-12 15:39:20 +0100

    Add material-tailwind library

--- front/app/root.tsx
+++ front/app/root.tsx
@@ -6,18 +6,13 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-
 import type { Route } from "./+types/root";
-import "./App.css";
+import "./app.css";
 import { AuthProvider } from "./components/auth/AuthContext";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
+  { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
@@ -33,7 +28,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning={true}>
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -61,7 +56,7 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
       error.status === 404
         ? "The requested page could not be found."
         : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
+  } else if (import.meta.env.DEV && error instanceof Error) {
     details = error.message;
     stack = error.stack;
   }
--- front/app/routes/auth/layout.tsx
+++ front/app/routes/auth/layout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router";
 
 export default function AuthLayout() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="w-full max-w-md p-6 rounded-2xl">
+    <div className="min-h-screen flex items-center justify-center p-6">
+      <div className="w-full max-w-md">
         <Outlet />
       </div>
     </div>
