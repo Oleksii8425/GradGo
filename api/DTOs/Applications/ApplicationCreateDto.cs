e6bd4e26e814045df0bf89a277e2cc31a10e2ed2
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-09-13 22:21:59 +0100

    Removed AppliedOn and Status fields from ApplicationCreateDto since they are defined by server

--- api/DTOs/Applications/ApplicationCreateDto.cs
+++ api/DTOs/Applications/ApplicationCreateDto.cs
@@ -5,8 +5,6 @@ namespace GradGo.DTOs
     public record ApplicationCreateDto
     (
         [Required] Guid JobId,
-        [Required] Guid JobseekerId,
-        [Required] DateTime AppliedOn,
-        [Required] string Status
+        [Required] Guid JobseekerId
     );
 }
