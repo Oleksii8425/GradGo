14f454e7098f6caf9c2dbd00d67ab2aa430b1d3f
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-09-13 15:53:41 +0100

    Add ApplicationStatus enum

---
+++ api/Models/ApplicationStatus.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace GradGo.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ApplicationStatus
+    {
+        [Description("Under Review")] UnderReview,
+        [Description("Successful")] Successful,
+        [Description("Rejected")] Rejected
+    }
+}
