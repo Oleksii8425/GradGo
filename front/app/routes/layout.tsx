22c5c50cbfb6aedecd6360e2e7f8d6f53d3682e9
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-10-07 14:37:54 +0100

    Update layout.tsx to set the height to screen height for the layout container

--- front/app/routes/layout.tsx
+++ front/app/routes/layout.tsx
@@ -3,7 +3,7 @@ import NavBar from "~/components/NavBar";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen w-screen flex flex-col">
+    <div className="h-screen w-screen flex flex-col">
       <NavBar />
       <div className="flex-1 flex justify-center items-center p-6">
         <Outlet />
